90596843db2f9e9209a08b253af66370109557b4
Catylist <henry.griffiths.dev@gmail.com>
2025-06-10 16:52:36 -0500

    .

--- startup.lua
+++ startup.lua
@@ -5,7 +5,7 @@ term.setCursorBlink(false)
 local monitors = { peripheral.find("monitor") }
 
 if #monitors > 0 then
-    term.redirect(monitor[1])
+    term.redirect(peripheral.wrap(monitor[1]))
 end
 
 DevMode = true -- Set to true to enable developer mode features
